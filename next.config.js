689f50a87234df75b353d35767bfc9fe3365f79b
Dmitry <endeveron@gmail.com>
2024-01-02 14:52:24 +0200

    Add content to onboarding page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  experimental: {
+    // serverActions: true,
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
