9c9f025921479648aa0c4eea5b1d73028a34c6c1
endeveron <endeveron@gmail.com>
2025-03-02 11:47:40 +0200

    update packages

--- components/shared/SuggestedCommunities.tsx
+++ components/shared/SuggestedCommunities.tsx
@@ -7,31 +7,31 @@ type TSuggestedCommunitiesProps = {};
 const SuggestedCommunities = async (props: TSuggestedCommunitiesProps) => {
   const communities = await fetchSuggestedCommunities({ number: 10 });
 
-  return (
-    communities?.length && (
-      <div className="suggested-communities flex flex-col gap-6">
-        {communities.map((community) => (
-          <div className="community-item" key={community.id}>
-            <Link href={`/community/${community.id}`}>
-              <Image
-                src={community.image}
-                alt="community_logo"
-                height={48}
-                width={48}
-                className="rounded-full object-cover"
-              />
+  return communities?.length ? (
+    <div className="suggested-communities flex flex-col gap-6">
+      {communities.map((community) => (
+        <div className="community-item" key={community.id}>
+          <Link href={`/community/${community.id}`}>
+            <Image
+              src={community.image}
+              alt="community_logo"
+              height={48}
+              width={48}
+              className="rounded-full object-cover"
+            />
 
-              <div>
-                <p className="text-small-medium text-main">{community.name}</p>
-                <p className="text-small-medium text-tertiary">
-                  @{community.username}
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    )
+            <div>
+              <p className="text-small-medium text-main">{community.name}</p>
+              <p className="text-small-medium text-tertiary">
+                @{community.username}
+              </p>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  ) : (
+    <p className="no-result">No communities found</p>
   );
 };
 
--- components/shared/SuggestedUsers.tsx
+++ components/shared/SuggestedUsers.tsx
@@ -13,31 +13,31 @@ const SuggestedUsers = async (props: TSuggestedUsersProps) => {
 
   const users = await fetchSuggestedUsers({ authUserId });
 
-  return (
-    users?.length && (
-      <div className="suggested-users flex flex-col gap-6">
-        {users.map((user) => (
-          <div className="user-item" key={user.id}>
-            <Link href={`/profile/${user.id}`}>
-              <Image
-                src={user.image}
-                alt="user avatar"
-                height={48}
-                width={48}
-                className="rounded-full object-cover"
-              />
+  return users?.length ? (
+    <div className="suggested-users flex flex-col gap-6">
+      {users.map((user) => (
+        <div className="user-item" key={user.id}>
+          <Link href={`/profile/${user.id}`}>
+            <Image
+              src={user.image}
+              alt="user avatar"
+              height={48}
+              width={48}
+              className="rounded-full object-cover"
+            />
 
-              <div>
-                <p className="text-small-medium text-main">{user.name}</p>
-                <p className="text-small-medium text-tertiary">
-                  @{user.username}
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    )
+            <div>
+              <p className="text-small-medium text-main">{user.name}</p>
+              <p className="text-small-medium text-tertiary">
+                @{user.username}
+              </p>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  ) : (
+    <p className="no-result">No users found</p>
   );
 };
 
